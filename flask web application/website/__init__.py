b438e8dfc394a61cec3e136459f191dc8b9de11c
Momothea <62673070+Momothea@users.noreply.github.com>
2023-02-27 17:53:48 +0100

    works

--- flask web application/website/__init__.py
+++ flask web application/website/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'bla bla bla'
+    metrics = PrometheusMetrics(app)
+    
 
     from .views import views
     from .auth import auth
