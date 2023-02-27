90c25e93c1d057983f0971dcf635e0f2ce84b369
Momothea <62673070+Momothea@users.noreply.github.com>
2023-02-27 11:22:57 +0100

    docker compose works

---
+++ Model/anime_model.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    json_data = request.get_json()
+    data = json.loads(json_data)
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
